35742989a37f16b8d8ddd4b6e4d3faa465ba93f5
xxngwang11 <779484643@qq.com>
2026-01-30 18:04:18 +0800

    Create AUv3TestHostApp.swift for AUv3TestHost plugin testing

---
+++ AUv3TestHost/AUv3TestHostApp.swift
@@ -0,0 +1,15 @@
+//  AUv3TestHostApp.swift
+//  AUv3TestHost
+//
+//  Simple AUv3 plugin test host for measuring load performance
+//
+import SwiftUI
+
+@main
+struct AUv3TestHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
